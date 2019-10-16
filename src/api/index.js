1f054c52c25106222dbe4619e01c2f3c2c9308b7
wuyiyin <wuyiyin@arhieason.com>
2019-10-16 17:57:27 +0800

    基本完成品类管理页面

--- src/api/index.js
+++ src/api/index.js
@@ -10,12 +10,12 @@ import jsonp from 'jsonp'
 // export function reqLogin(username,password){
 //     return ajax('/Login',{username,password},'POST')
 // }
+const BASE = ''
 
-
-export const reqLogin = (username, password) => ajax('/Login', { username, password }, 'POST')
+export const reqLogin = (username, password) => ajax(BASE + '/Login', { username, password }, 'POST')
 
 //添加用户
-export const reqAddUser = (user) => ajax('/manage/user/add', user, 'POST')
+export const reqAddUser = (user) => ajax(BASE + '/manage/user/add', user, 'POST')
 
 /*
 jsonp请求的接口请求函数
@@ -30,7 +30,7 @@ export const reqWeather = (city) => {
             //如果成功了
             if (!err && data.status === 'success') {
                 const { dayPictureUrl, weather } = data.results[0].weather_data[0]
-                resolve({dayPictureUrl, weather})
+                resolve({ dayPictureUrl, weather })
             } else {
                 //如果失败了
                 message.error('获取天气信息失败！')
@@ -41,3 +41,14 @@ export const reqWeather = (city) => {
 
 }
 // reqWeather('成都')
+
+
+
+//获取一级/二级分类的列表
+export const reqCategorys = (parentId) => ajax(BASE + '/manage/category/list', { parentId })
+
+//添加分类
+export const reqAddCategorys = (categoryName, parentId) => ajax(BASE + '/manage/category/add', { categoryName, parentId }, 'POST')
+
+//更新分类
+export const reqUpdateCategorys = ({ categoryId, categoryName }) => ajax(BASE + '/manage/category/add', { categoryId, categoryName }, 'POST')
--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -1,93 +1,139 @@
 import React, { Component } from 'react'
-import { Card, Table, Button, Icon} from 'antd'
+import { Card, Table, Button, Icon, message ,Modal} from 'antd'
 import LinkButton from '../../components/link-button'
-
+import { reqCategorys } from '../../api'
 //商品分类路由
 
 
 export default class Category extends Component {
- 
-    render() {
-      const dataSource = [
-        {
-          "parentId":"0",
-          "_id":"5ca9d695ef916541160ba",
-          "name":"家用电器",
-          "__v":"0"
-        },
-        {
-          "parentId":"0",
-          "_id":"5ca9d695ef916541160bb",
-          "name":"电脑",
-          "__v":"0"
-        },
-        {
-          "parentId":"0",
-          "_id":"5ca9d695ef916541160bc",
-          "name":"图书",
-          "__v":"0"
-        },
-        {
-          "parentId":"0",
-          "_id":"5ca9d695ef916541160be",
-          "name":"服装",
-          "__v":"0"
-        },
-        {
-          "parentId":"0",
-          "_id":"5ca9d695ef916541160bf",
-          "name":"食品",
-          "__v":"0"
-        },
-        {
-          "parentId":"0",
-          "_id":"5ca9d695ef916541160c0",
-          "name":"医药",
-          "__v":"0"
-        },
-        {
-          "parentId":"0",
-          "_id":"5ca9d695ef916541160c1",
-          "name":"汽车产品",
-          "__v":"0"
-        },
-        {
-          "parentId":"0",
-          "_id":"5ca9d695ef916541160c2",
-          "name":"箱包",
-          "__v":"0"
-        }
-      ];
-      
-      const columns = [
-        {
-          title: '分类的名称',
-          dataIndex: 'name',//指定显示对应数据的属性名
-        },
-        {
-          title: '操作',
-          render:()=>(//返回需要显示的界面标签
-            <span>
-              <LinkButton>修改分类</LinkButton>
-              <LinkButton>查看子分类</LinkButton>
-            </span>
-          )
-            
-        },
-      ];
-        const title='一级分类列表'
-        const extra =(
-        <Button type='primary'>
-            <Icon type='plus'></Icon>
-            <span>添加</span>
-        </Button>)
-        return (
-            <Card title={title} extra={extra}>
-                <Table 
-                bordered
-                dataSource={dataSource}
-                columns={columns} />;
-            </Card>
+  state = {
+    loading: false,
+    categorys: [],//一级分类列表
+    subCategorys: [],//二级分类列表
+    parentId: '0',//当前需要显示的分类列表的Id
+    parentName: ''
+  }
+  //初始化所有列的数组
+
+  initColumns = () => {
+    this.columns = [
+      {
+        title: '分类的名称',
+        dataIndex: 'name',//指定显示对应数据的属性名
+      },
+      {
+        title: '操作',
+        width: 300,
+        render: (category) => (//返回需要显示的界面标签
+          <span>
+            <LinkButton>修改分类</LinkButton>
+            {/* 如何向事件回调函数传递参数： 先定义一个匿名函数，在函数调用处理的函数并传入数据 */}
+
+
+            {this.state.parentId === '0' ? <LinkButton onClick={() => { this.showSubCategorys(category) }}>查看子分类</LinkButton> : null}
+
+          </span>
         )
+
+      },
+    ];
+  }
+
+  getCategorys = async () => {
+    //发请求前，显示loading 
+    this.setState({ loading: true })
+    //发异步ajax获取数据
+    const { parentId } = this.state
+    const reslut = await reqCategorys(parentId)
+
+
+    //请求完成后，隐藏loading
+    this.setState({ loading: false })
+    // console.log('reslut.state'+reslut.state)
+
+    if (reslut.state === 0) {
+      //取出分类数组（可能一级也可能二级）
+      const categorys = reslut.data
+
+      if (parentId === '0') {
+        this.setState({
+          categorys: categorys
+        })
+      } else {
+        this.setState({
+          subCategorys: categorys
+        })
+      }
+
+    } else {
+      message.error('获取分类列表失败')
     }
+  }
+
+
+  //显示指定一级分类的二级列表
+  showSubCategorys = (category) => {
+    this.setState({
+      parentId: category._id,
+      parentName: category.name
+    },
+
+      () => {
+        //在状态更新且重新render()后执行
+        // console.log('parentId',this.state.parentId)
+        //获取二级分类列表
+        this.reqCategorys()
+      })
+    //setState()不能立即获取更新的状态；因为setState()是异步更新状态的
+    // console.log('parentId',this.state.parentId)//'0'
+  }
+  //显示指定一级分类列表
+  showCategorys = () => {
+    //更新为显示一级列表的状态
+    this.setState({
+      parentId: '0',
+      parentName: '',
+      subCategorys: []
+    })
+  }
+
+  //为第一次render()准备数据
+  componentWillMount() {
+    this.initColumns()
+  }
+  //发异步ajax请求
+  componentDidMount() {
+    //获取一级分类列表
+    this.getCategorys()
+
+  }
+
+  render() {
+    //读取状态数据   更新后一定要记得读取
+    const { loading, categorys, subCategorys, parentName, parentId } = this.state
+
+    const title = parentId === '0' ? '一级分类列表' : (
+      <span>
+        <LinkButton onClick={this.showCategorys}>一级分类列表</LinkButton>
+        <Icon type='arrow-right' style={{ marginRight: 5 }}></Icon>
+        <span>{parentName}</span>
+      </span>
+    )
+    const extra = (
+      <Button type='primary'>
+        <Icon type='plus'></Icon>
+        <span>添加</span>
+      </Button>)
+    return (
+      <Card title={title} extra={extra}>
+        <Table
+          bordered
+          rowKey='_id'
+          loading={loading}
+          dataSource={parentId === '0' ? categorys : subCategorys}
+          columns={this.columns}
+          pagination={{ defaultPageSize: 5, showQuickJumper: true }} />;
+            </Card>
+    )
+  }
 }
